0708fc740ce7e3327a87c1d78544449cb54afafa
movope <mail@movope.de>
2014-03-30 14:31:29 +0200

    move main() into ChessGame

--- src/de/movope/game/ChessGame.java
+++ src/de/movope/game/ChessGame.java
@@ -26,4 +26,21 @@ public class ChessGame {
         System.out.println();
         System.out.println();
     }
+
+    public static void main(String[] arg) {
+        ChessGame game = new ChessGame();
+        game.init();
+
+        Player white = game.getWhitePlayer();
+        Player black = game.getBlackPlayer();
+
+        game.print();
+
+        for (int i = 0; i < 4; i++) {
+            white.makeRandomMove();
+            game.print();
+            black.makeRandomMove();
+            game.print();
+        }
+    }
 }
