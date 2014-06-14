c9c66ce315412d7fe42d9fb9194035631de99a7f
movope <mail@movope.de>
2014-06-14 21:00:32 +0200

    remove typo

--- testSrc/de/movope/game/PlayerTest.groovy
+++ testSrc/de/movope/game/PlayerTest.groovy
@@ -12,7 +12,7 @@ class PlayerTest extends Specification {
         board.initPieces();
     }
 
-    def "checkmate is detected and player react correctly"() {
+    def "checkmate is detected and player reacts correctly"() {
         when:
         board.move("F1", "H5")
         board.move("F7", "F5")
