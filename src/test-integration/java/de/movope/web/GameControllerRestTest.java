707959d653a225ba7423d8132c7d624436444df1
movope <mail@movope.de>
2015-01-01 13:25:47 +0100

    add integration-test that checks for correct status code, if game does not exist

---
+++ src/test-integration/java/de/movope/web/GameControllerRestTest.java
@@ -0,0 +1,41 @@
+package de.movope.web;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class GameControllerRestTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ChessGameRepository chessGameRepository;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+        this.chessGameRepository.deleteAll();
+    }
+
+    @Test
+    public void gameNotFound() throws Exception {
+        mockMvc.perform(get("/game/notexisting"))
+                .andExpect(status().isNotFound());
+    }
+}
