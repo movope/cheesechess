34eff5c5f507ff2cc36a65577be1e356686c7bf8
movope <mail@movope.de>
2015-01-02 14:11:55 +0100

    add integration-test for executing a move

--- src/test-integration/java/de/movope/web/GameControllerRestTest.java
+++ src/test-integration/java/de/movope/web/GameControllerRestTest.java
@@ -1,14 +1,14 @@
 package de.movope.web;
 
 import de.movope.game.ChessBoard;
+import de.movope.game.Move;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class GameControllerRestTest extends SystemTest {
@@ -38,6 +38,35 @@ public class GameControllerRestTest extends SystemTest {
 
         ChessBoardView response = fromJson(result, ChessBoardView.class);
         assertEquals(new ChessBoardView(ChessBoard.createNew(gameId)), response);
-
     }
+
+    @Test
+    public void aValidMoveCanBeExecuted() throws Exception {
+        String gameId = "ben";
+        mockMvc.perform(put("/game/" + gameId)).andExpect(status().isCreated());
+
+        Move move = Move.create("A2", "A4");
+
+        mockMvc.perform(post("/game/{gameId}/move", gameId)
+                .content(this.json(toResource(move)))
+                .contentType(contentType))
+                .andExpect(status().isOk());
+
+        ChessBoard expected = ChessBoard.createNew("ben");
+        expected.execute(move);
+
+        MvcResult result = mockMvc.perform(get("/game/{gameId}/board", gameId))
+                .andExpect(status().isOk())
+                .andReturn();
+        ChessBoardView response = fromJson(result, ChessBoardView.class);
+        assertEquals(new ChessBoardView(expected), response);
+    }
+
+    private MoveRessource toResource(Move move) {
+        MoveRessource ressource = new MoveRessource();
+        ressource.setTo(move.to().toString());
+        ressource.setFrom(move.from().toString());
+        return ressource;
+    }
+
 }
--- src/test-integration/java/de/movope/web/SystemTest.java
+++ src/test-integration/java/de/movope/web/SystemTest.java
@@ -1,10 +1,15 @@
 package de.movope.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,6 +17,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
@@ -20,10 +27,26 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @WebAppConfiguration
 public class SystemTest {
 
+    protected MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
     protected MockMvc mockMvc;
+
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
 
     @Autowired
     protected WebApplicationContext webApplicationContext;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream().filter(
+                hmc -> hmc instanceof MappingJackson2HttpMessageConverter).findAny().get();
+
+        Assert.assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
 
     @Before
     public void setup() throws Exception {
@@ -38,5 +61,12 @@ public class SystemTest {
             e.printStackTrace();
         }
         return null;
+    }
+
+    protected String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
     }
 }
