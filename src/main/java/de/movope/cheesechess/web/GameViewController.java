45c91173f290729ca6c6b77b25470b133a08a877
movope <mail@movope.de>
2016-02-25 14:32:17 +0100

    you can make a move now, so let's play

--- src/main/java/de/movope/cheesechess/web/GameViewController.java
+++ src/main/java/de/movope/cheesechess/web/GameViewController.java
@@ -2,17 +2,18 @@ package de.movope.cheesechess.web;
 
 
 import de.movope.cheesechess.web.api.ChessBoardView;
+import de.movope.cheesechess.web.api.MoveResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class GameViewController {
@@ -36,6 +37,17 @@ public class GameViewController {
         model.addAttribute("gameId", gameId);
         ChessBoardView board = gameService.getBoardFromGame(gameId);
         model.addAttribute("pieces", board.getBoardView());
+        model.addAttribute("move", new MoveResource());
         return "game";
+    }
+
+
+    @RequestMapping(value = "/game/{gameId}/move/new", method = POST)
+    public String makeMove(@PathVariable(value = "gameId") String gameId,
+                           @ModelAttribute(value = "move") MoveResource move,
+                           Model model) {
+        model.addAttribute("move", move);
+        gameService.makeMoveWithWhitePlayer(gameId, move);
+        return "redirect:/game/" + gameId;
     }
 }
