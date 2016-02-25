889c27d8ab6fec7714eabd0cb0507eda2bebb6b0
movope <mail@movope.de>
2016-02-25 10:25:13 +0100

    add template for a chess game

--- src/main/java/de/movope/cheesechess/web/GameViewController.java
+++ src/main/java/de/movope/cheesechess/web/GameViewController.java
@@ -4,9 +4,14 @@ package de.movope.cheesechess.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 public class GameViewController {
@@ -14,12 +19,20 @@ public class GameViewController {
     @Autowired
     GameService gameService;
 
-    @RequestMapping("/game/overview")
+    @RequestMapping(value = "/game/overview", method = GET)
     public String index(Model model) {
 
         List<String> gameIds = gameService.getAllGameIds();
         model.addAttribute("gameIds", gameIds);
 
         return "index";
+    }
+
+
+    @RequestMapping(value = "/game/{gameId}", method = GET)
+    public String game(Model model, @PathVariable(value = "gameId") String gameId) {
+
+        model.addAttribute("gameId", gameId);
+        return "game";
     }
 }
